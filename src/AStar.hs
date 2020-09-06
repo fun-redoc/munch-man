70aa7ffa3cc4b75c3b3745c0aecf550d7c1bd6ae
fun-redoc <fun.redoc@gmail.com>
2020-09-06 17:32:22 +0200

    A* with bug

---
+++ src/AStar.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE BangPatterns #-}
+module AStar (astar, Heuristics, euclidian, RPQ) where
+
+import Debug.Trace (trace)
+
+import Graph as G 
+import WGraph as WG
+import PriorityQueue as PQ
+import Control.Monad.Trans.State.Lazy (put, get, StateT)
+import Data.Map as M
+import Control.Monad.Loops
+import Data.Maybe (fromMaybe, isNothing, fromJust)
+import Data.Traversable (for)
+import Control.Monad (join, forM_, forM)
+import Data.Hashable (Hashable)
+import Data.List (sortOn, nubBy)
+
+data RPQ t p a = RPQ (t (a,p)) 
+instance (Show a, Eq a,  Ord p)=>PriorityQueue RPQ [] p a where
+    emptyPriorityQueue = RPQ []
+    is_empty (RPQ pq) = Prelude.null pq
+    insert_with_priority (RPQ pq) (e,p) = RPQ $ nubBy (\(x,_) (y,_) -> if x /= y 
+                                                                 then False 
+                                                                 else True)
+                                            $ reverse 
+                                            $ sortOn snd ((e,p):pq)
+    pull_highest_priority_element pq@(RPQ [])= (Nothing, pq)
+    pull_highest_priority_element (RPQ (x:xs))= (Just x, RPQ xs)
+    update_weight (RPQ xs) x p = RPQ $ sortOn snd $ Prelude.map (\(x',p') -> if x == x' then (x,p) else (x', p')) xs
+instance (Show p, Show a)=>Show (RPQ [] p a) where
+    show (RPQ pq) = show pq
+
+type Heuristics g a n = Num n=>(Graph g a)=>g a->a->a->n
+
+euclidian::Heuristics g (Float, Float) Float
+euclidian g (x1,y1) (x2,y2) = sqrt (dx*dx + dy*dy)
+                                where dx = x2-x1
+                                      dy = y2-y1
+
+backtrack::(Ord a, Show a, Eq a, Hashable a, Applicative t, Foldable t, Monoid (t a))=>a->a->M.Map a (Maybe a, n)->t a
+backtrack start dest dist_mat = iterate dest (pure dest) 
+    where iterate dest' res = 
+            if (dest' ==  start) || (isNothing prev_vert)
+                then res 
+                else iterate (fromJust prev_vert) res' 
+                where
+                    prev_dist = dist_mat M.!? dest'
+                    prev_vert = join $ fst <$> prev_dist
+                    res' = (pure $ fromJust prev_vert) <> res
+
+-- | 1 to 1 monadic translation of the imperative algorithm
+--astar::(Show (pq t (Infinite n) a), Show n, Hashable a, Show a, Bounded n, Num n, Ord a, Eq a, WGraph g n a, Graph (g n) a, PriorityQueue pq t (Infinite n) a, Monad m)
+--     -- Heuristics (g a) a n -> a -> a 
+--     =>(g n a->a->a->(Infinite n))->a->a 
+--     ->StateT (pq t (Infinite n) a, Map a (Maybe a, (Infinite n)), g n a) m [a]
+astar heuristics startVertex destVertex = 
+    do
+        (open, closed, graph) <- get
+        let open' = insert_with_priority open (startVertex, PositiveInfinity)
+        let closed' = M.insert startVertex (Nothing, Bound 0) closed
+        put (open', closed', graph)
+        whileM_ (do (open,_,_) <- get
+                    let (cur,_) = pull_highest_priority_element open
+                    return $ maybe False (/=destVertex) $ fst <$> cur
+                )
+                (do 
+                    (open, closed, graph) <- get
+                    let (cur, open') = pull_highest_priority_element open
+                    put (open', closed, graph)
+                    if isNothing cur
+                        then return ()
+                        else do
+                            let cur' = fst $ fromJust cur
+                            let neighboursWithWeight = WG.adjacent_vertices graph cur'
+                            forM_ neighboursWithWeight 
+                                (\(next, costToNext') -> do  -- use second arg as costToNext
+                                    (open, closed, graph) <- get
+                                    let !costSoFar = snd <$> closed M.!? cur'
+                                    let !costToNext = get_weight graph cur' next
+                                    let !newCost = (+) <$> costSoFar <*> (Just costToNext)
+                                    let !oldCost  = snd <$> (closed M.!? next)
+                                    let !newCostIsLowerThanOldCost = (<) <$> newCost <*> oldCost
+                                    if fromMaybe True newCostIsLowerThanOldCost
+                                        then do
+--                                    if (not $ next `M.member` closed) || (newCost < (snd $ closed M.! next))
+--                                        then do 
+                                                let newCost' = fromJust newCost
+                                                let !closed' = M.alter (const $ Just (Just cur', newCost')) next closed
+                                                let !prio = newCost' + (heuristics graph destVertex next)
+                                                let !open' = insert_with_priority open (next, prio)
+                                                put (open', closed', graph)
+                                                return ()
+                                        else return ()
+                                )
+                            return ()
+                )
+
+        (open, closed, _) <- get
+        return $ (trace (show ("backtrack", closed))) $ (backtrack startVertex destVertex closed)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,40 +1,145 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE ScopedTypeVariables, BangPatterns #-}
 import Graph
 import Lib
 import Board
-import Data.Graph.Inductive.Graph (Node, mkGraph, lab, labNodes)
-import Data.Graph.Inductive.PatriciaTree (Gr, UGr)
-import Data.Graph.Inductive.Query.SP (sp, spLength, dijkstra)
+--import Data.Graph.Inductive.Graph (Node, mkGraph, lab, labNodes)
+--import Data.Graph.Inductive.PatriciaTree (Gr, UGr)
+--import Data.Graph.Inductive.Query.SP (sp, spLength, dijkstra)
 import Data.List (elemIndex, nub, sort,find)
 import Data.Maybe (isJust, fromJust)
 import qualified Data.Array.Repa as R
 import Data.Graph.Inductive (undir)
+import Graph as G 
+import WGraph as WG
+import PriorityQueue as PQ
+import Control.Monad.Trans.State.Lazy -- (put, get, StateT)
+import Data.Map as M
+import Data.HashMap.Lazy as HM
+import Control.Monad.Loops
+import Data.Maybe (isNothing, fromJust)
+import Data.Traversable (for)
+import Control.Monad (forM_, forM)
+import qualified Data.Vector as V
+import qualified Data.Array.Repa as R
+import qualified Data.Array.Repa.Eval as R
+import qualified Data.Array.Repa.Shape as R
+import qualified Data.Array.Repa.Repr.Vector as R
+
+import AStar
+import GHC.Base (join)
+
+data RG p a = RG {unRG::(R.Array R.V R.DIM2) (Maybe (a, p))}
+
+instance (Num n)=>Graph (RG  n) Pos where
+  emptyGraph       = undefined
+  num_vertices     = undefined
+  adjacent_vertices= undefined
+  all_nodes        = undefined
+  add_vertex       = undefined
+
+instance Num n=>WGraph RG n Pos where
+  get_weight (RG g) v1 v2    = 1 
+  adjacent_vertices (RG g) p = adjacent_vertices_RG g p
+
+adjacent_vertices_RG g (x,y) = let [nRows, nCols] = R.listOfShape $ R.extent g
+                                   xMinus1 = x - 1
+                                   xPlus1  = x + 1
+                                   yMinus1  = y - 1
+                                   yPlus1  = y + 1
+                                   xLeft   = if xMinus1 == -1 then nCols-1 -- tunnel
+                                                             else xMinus1
+                                   xRight  = if xPlus1 == nCols then 0 -- tunnel
+                                                               else xPlus1
+                                   yUp     = if yPlus1 == nRows then 0 -- tunnel
+                                                               else yPlus1
+                                   yDown   = if yMinus1 == -1 then nRows-1 -- tunnel
+                                                             else yMinus1
+                                   leftNeighbour  = g R.! (R.ix2 y     xLeft )
+                                   rightNeighbour = g R.! (R.ix2 y     xRight)
+                                   upperNeighbour = g R.! (R.ix2 yUp   x)
+                                   lowerNeighbour = g R.! (R.ix2 yDown x)
+                                in -- (\ns -> (trace (show ns)) ns) $ 
+                                  (Prelude.map fromJust . Prelude.filter isJust) $ [leftNeighbour, rightNeighbour, upperNeighbour, lowerNeighbour]
+
+toRepaDIM2_RG::[String] -> RG (Infinite Float) Pos 
+toRepaDIM2_RG xs = RG $ R.computeS $ R.traverse
+                  (R.fromListUnboxed (R.Z R.:. (rows::Int) R.:. (cols::Int)) (join $ reverse xs)) -- TODO take sizes from configuration
+                  id
+                  (\f (R.Z R.:. y R.:. x)->extract ((x,y),f (R.ix2 y x)))
+    where extract ((x,y),c) = -- (trace $ show ("Board", x,y,c)) $
+            if (c == 'O' || c == '.' || c == '_' || c == 'G' || c == '@')
+                then Just ((x,y),1)
+                else Nothing
+          rows = length xs
+          cols = if rows > 0 then (length $ xs !! 0) else 0
+
+-- euclidian1::(Integral n, Num n)=>Heuristics g (n, n) (Infinite Float)
+euclidian1 g (x1,y1) (x2,y2) = sqrt' $ fromIntegral $ (dx*dx + dy*dy)
+                               where dx = x2-x1
+                                     dy = y2-y1
+                                     sqrt' (Bound x) = Bound (sqrt x)
+                                     sqrt' PositiveInfinity = PositiveInfinity
+                                     sqrt' NegativeInfinity = undefined
 
 
+test::[String]
+test = ["XXXXXXXXXXX"--4
+       ,"X_________X"--3
+       ,"X___XX____X"--2
+       ,"X_________X"--1
+       ,"XXXXXXXXXXX"--0
+       --01234567890
+       ]
+
+instance Bounded (Infinite n) where
+  maxBound = PositiveInfinity
+  minBound = NegativeInfinity
 
 main :: IO ()
 main = do
-    let !repa = toRepaDIM2 board
-    let ns::[Pos] = map snd $ map fromJust $ filter isJust (R.toList (unGG repa))
-    let vertices = nub 
-                 $ foldr 
-                    (\n vs -> let neighbours = map snd $ adjacent_vertices_GG repa n
-                                  vs' = sort $ map (\p -> (n,p)) neighbours 
-                              in  vs'++vs
-                    ) []
-                      ns
-    let g::Gr Pos Int = undir $ mkGraph
-                            (map (\n->(fromJust $ elemIndex n ns, n)) ns)
-                            (map (\(n1,n2)->(fromJust $ elemIndex n1 ns, fromJust $ elemIndex n2 ns, 1)) vertices)
-    let (startLabel, destLabel) = ((17,12), (17,1))
-    let start::Node = fromJust $ elemIndex startLabel ns
-    let dest ::Node = fromJust $ elemIndex destLabel ns
-    let ln = labNodes g
-    let node lab = fst $ fromJust $ find (\(_,l)->l==lab) ln
-    let start'::Node = node startLabel
-    let dest' ::Node = node destLabel
-    print (start, dest)
-    -- print $ (map (lab g)) <$>  (sp start dest g)
-    print $ fromJust $ (map (fromJust . lab g)) <$> sp start dest g
-    print $ fromJust $ (map (fromJust . lab g)) <$> sp start' dest' g
+    let !repa = toRepaDIM2_RG test
+    let a1::StateT (PQ [] (Infinite Float) Pos, M.Map Pos (Maybe Pos,(Infinite Float)),RG (Infinite Float) Pos) IO [Pos] 
+            = astar euclidian1 (1,1) (9,3)
+    print "before"
+    print $ WG.adjacent_vertices repa (1,1)
+    print "before 1"
+    !astarPath <- evalStateT a1 
+                             ( emptyPriorityQueue::PQ [] (Infinite Float) Pos
+                             , M.empty::M.Map Pos (Maybe Pos,(Infinite Float))
+                             , repa
+                             )
+    print "after"
+    print astarPath
     return ()
+
+--main = do
+--    let !repa = toRepaDIM2 board
+--    let ns::[Pos] = map snd $ map fromJust $ filter isJust (R.toList (unGG repa))
+--    let vertices = nub 
+--                 $ foldr 
+--                    (\n vs -> let neighbours = map snd $ adjacent_vertices_GG repa n
+--                                  vs' = sort $ map (\p -> (n,p)) neighbours 
+--                              in  vs'++vs
+--                    ) []
+--                      ns
+--    let g::Gr Pos Int = undir $ mkGraph
+--                            (map (\n->(fromJust $ elemIndex n ns, n)) ns)
+--                            (map (\(n1,n2)->(fromJust $ elemIndex n1 ns, fromJust $ elemIndex n2 ns, 1)) vertices)
+--    let (startLabel, destLabel) = ((17,12), (17,1))
+--    let start::Node = fromJust $ elemIndex startLabel ns
+--    let dest ::Node = fromJust $ elemIndex destLabel ns
+--    let ln = labNodes g
+--    let node lab = fst $ fromJust $ find (\(_,l)->l==lab) ln
+--    let start'::Node = node startLabel
+--    let dest' ::Node = node destLabel
+--    print (start, dest)
+--    -- print $ (map (lab g)) <$>  (sp start dest g)
+--    print $ fromJust $ (map (fromJust . lab g)) <$> sp start dest g
+--    print $ fromJust $ (map (fromJust . lab g)) <$> sp start' dest' g
+--    return ()
+--(Num n, Integral n)(Num n, Integral n)
